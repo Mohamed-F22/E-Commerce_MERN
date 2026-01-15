719b84ac14740bead9773a4b614fd6b0184b5765
Mohamed Amr <126739293+Mohamed-F22@users.noreply.github.com>
2026-01-15 15:35:10 +0200

    Update endPoint for /cart/items Put

--- src/services/cartService.ts
+++ src/services/cartService.ts
@@ -18,7 +18,6 @@ interface getActiveCart {
 
 export const getActiveCart = async ({userId} : getActiveCart) => {
   let cart = await cartModel.findOne({userId, status: "active"})
-  // let cart = undefined
 
   if (!cart) {
     let cart = await createCartForUser({userId})
@@ -37,7 +36,6 @@ interface addItemToCart {
 export const addItemToCart = async ({userId, productId, quantity}: addItemToCart) => {
   const cart = await getActiveCart({userId})  
 
-  // const existsInCart = cart.items.find((p) => p.product === productId)
   const existsInCart = cart.items.find((p) => p.product.toString() === productId.toString());
 
   if (existsInCart) {
@@ -61,6 +59,51 @@ export const addItemToCart = async ({userId, productId, quantity}: addItemToCart
   })
 
   cart.totalAmount += product.price * quantity
+
+  const updatedData = await cart.save()
+  return {data: updatedData, statusCode: 200}
+}
+
+interface updateItemInCart {
+  productId: any,
+  quantity: number,
+  userId: string
+}
+
+export const updateItemInCart = async ({userId, productId, quantity}: updateItemInCart) => {
+  const cart = await getActiveCart({userId})
+
+  const existsInCart = cart.items.find((p) => p.product.toString() === productId.toString());
+
+  console.log(existsInCart);
+  
+
+  if (!existsInCart) {
+    return {data: "Item dose not exist in cart!", statusCode: 400}
+  }
+
+    const product = await productModel.findById(productId)
+
+  if(!product) {
+    return {data: "Product not found!", statusCode: 400}
+  }
+
+  if (quantity > product.stock) {
+    return {data: "This quantity is not provided!", statusCode: 400}
+  }
+
+  
+  const otherCartItems = cart.items.filter(((p) => p.product.toString() !== productId))
+  
+  let total = otherCartItems.reduce((sum, product) => {
+    sum += product.quantity * product.unitPrice
+    return sum
+  }, 0)
+
+  existsInCart.quantity = quantity
+
+  total += existsInCart.quantity * existsInCart.unitPrice
+  cart.totalAmount = total
 
   const updatedData = await cart.save()
   return {data: updatedData, statusCode: 200}
